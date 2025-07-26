f6d13d908e3924532611ce18dd8d1db8c32c7dce
dexin <dexinte@163.com>
2025-07-26 16:57:54 +0800

    finish

--- predict_result.py
+++ predict_result.py
@@ -1,4 +1,7 @@
+from ast import parse
 import os
+import pandas as pd
+from tqdm import tqdm
 import trimesh
 import yaml
 import numpy as np
@@ -6,6 +9,12 @@ import cv2
 import torch
 
 from PIL import Image
+from CoordAR.data.bop.few_shot_dataset import BOPFewShot
+from CoordAR.data.bop.instance_dataset import BOPInstanceDataset
+from CoordAR.data.bop.scene_dataset import BOPSceneDataset
+from CoordAR.data.megapose.obj_ds.bop_object_dataset import BOPObjectDataset
+from CoordAR.utils.logging import get_logger
+from CoordAR.utils.misc import prepare_dir
 from estimater import Any6D
 
 from foundationpose.Utils import (
@@ -22,87 +31,126 @@ from sam2_instantmesh import *
 
 glctx = dr.RasterizeCudaContext()
 
+logger = get_logger(__name__)
+
 if __name__ == "__main__":
 
     seed_everything(0)
 
-    parser = argparse.ArgumentParser(description="Set experiment name and paths")
+    parser = argparse.ArgumentParser(description="Predict on BOP")
     parser.add_argument(
-        "--ycb_model_path",
+        "--datasets_path",
         type=str,
-        default="dataset/ho3d/YCB_Video_Models/models",
-        help="Path to the YCB Video Models",
+        help="Path to the datasets directory",
+        default="data/BOP",
     )
+    parser.add_argument(
+        "--dataset", type=str, help="Name of the dataset to partition", default="lm"
+    )
+    parser.add_argument(
+        "--split",
+        type=str,
+        help="Dataset split to partition",
+        default="test",
+    )
+    parser.add_argument("--split_type", default="none", help="Type of dataset split")
     parser.add_argument(
         "--img_to_3d", action="store_true", help="Running with InstantMesh+SAM2"
     )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        help="Directory to save the output results",
+        default="logs/predict",
+    )
     args = parser.parse_args()
 
-    ycb_model_path = args.ycb_model_path
+    """
+        init dataset
+    """
+    scene_dataset = BOPSceneDataset(
+        args.datasets_path,
+        args.dataset,
+        args.split,
+        args.split_type if args.split_type != "none" else None,
+        only_bop19_test=True,
+    )
+    obj_ds = BOPObjectDataset(f"{args.datasets_path}/{args.dataset}/models")
+    instance_dataset = BOPInstanceDataset(scene_dataset, obj_ds)
+    few_shot_ds = BOPFewShot(
+        instance_dataset,
+        num_ref=1,
+        ref="first_frame",
+    )
+
     img_to_3d = args.img_to_3d
 
-    results = []
-    demo_path = "demo_data"
-    mesh_path = os.path.join(demo_path, f"mustard.obj")
-
-    obj = "demo_mustard"
-    save_path = f"results/{obj}"
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-
-    depth_scale = 1000.0
-    color = cv2.cvtColor(
-        cv2.imread(os.path.join(demo_path, "color.png")), cv2.COLOR_BGR2RGB
-    )
-    depth = (
-        cv2.imread(os.path.join(demo_path, "depth.png"), cv2.IMREAD_ANYDEPTH).astype(
-            np.float32
-        )
-        / depth_scale
-    )
-    Image.fromarray(color).save(os.path.join(save_path, "color.png"))
-
-    label = np.load(os.path.join(demo_path, "labels.npz"))
-    obj_num = 5
-    mask = np.where(label["seg"] == obj_num, 255, 0).astype(np.bool_)
-
+    # build model for each object
+    model_out = f"{args.output_dir}/{args.dataset}_{args.split}/model"
+    prepare_dir(model_out)
     if img_to_3d:
-        cmin, rmin, cmax, rmax = get_bounding_box(mask).astype(np.int32)
-        input_box = np.array([cmin, rmin, cmax, rmax])[None, :]
-        mask_refine = running_sam_box(color, input_box)
+        for sample in tqdm(few_shot_ds):
+            obj_id = sample["obj_id"]
+            obj_name = f"{obj_id:06d}"
+            if os.path.exists(os.path.join(model_out, f"center_mesh_{obj_name}.obj")):
+                logger.info(f"center_mesh_{obj_name}.obj found, skip generation")
+                break
+            else:
+                mesh_path = os.path.join(model_out, f"{obj_id:06d}.obj")
+                mask = sample["template_masks_visib"][0]
+                color = rearrange(sample["template_imgs"][0], "c h w -> h w c")
+                Image.fromarray(color).save(os.path.join(model_out, "color.png"))
 
-        input_image = preprocess_image(color, mask_refine, save_path, obj)
-        images = diffusion_image_generation(
-            save_path, save_path, obj, input_image=input_image
+                """ call 123 model"""
+                cmin, rmin, cmax, rmax = get_bounding_box(mask).astype(np.int32)
+                input_box = np.array([cmin, rmin, cmax, rmax])[None, :]
+                mask_refine = running_sam_box(color, input_box)
+
+                input_image = preprocess_image(color, mask_refine, model_out, obj_name)
+                images = diffusion_image_generation(
+                    model_out, model_out, obj_name, input_image=input_image
+                )
+                instant_mesh_process(images, model_out, obj_name)
+
+                mesh = trimesh.load(os.path.join(model_out, f"mesh_{obj_name}.obj"))
+                mesh = align_mesh_to_coordinate(mesh)
+                mesh.export(os.path.join(model_out, f"center_mesh_{obj_name}.obj"))
+
+    pose_out = f"{args.output_dir}/{args.dataset}_{args.split}/pose"
+    prepare_dir(pose_out)
+    predictions = []
+    for sample in tqdm(few_shot_ds):
+        obj_id = sample["obj_id"]
+        intrinsic = sample["query_K_crop"]
+        obj_name = f"{obj_id:06d}"
+        mask = sample["template_masks_visib"][0]
+        color = rearrange(sample["template_imgs"][0], "c h w -> h w c")
+        depth = sample["query_depth"]
+
+        mesh = trimesh.load(os.path.join(model_out, f"center_mesh_{obj_name}.obj"))
+
+        est = Any6D(symmetry_tfs=None, mesh=mesh, debug_dir=pose_out, debug=2)
+
+        pred_pose = est.register_any6d(
+            K=intrinsic,
+            rgb=color,
+            depth=depth,
+            ob_mask=mask,
+            iteration=5,
+            name=obj_name,
         )
-        instant_mesh_process(images, save_path, obj)
 
-        mesh = trimesh.load(os.path.join(save_path, f"mesh_{obj}.obj"))
-        mesh = align_mesh_to_coordinate(mesh)
-        mesh.export(os.path.join(save_path, f"center_mesh_{obj}.obj"))
-
-        mesh = trimesh.load(os.path.join(save_path, f"center_mesh_{obj}.obj"))
-    else:
-        mesh = trimesh.load(mesh_path)
-
-    est = Any6D(symmetry_tfs=None, mesh=mesh, debug_dir=save_path, debug=2)
-
-    # camera info
-    intrinsic_path = f"{demo_path}/836212060125_640x480.yml"
-    with open(intrinsic_path, "r") as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
-
-    intrinsic = np.array(
-        [
-            [data["depth"]["fx"], 0.0, data["depth"]["ppx"]],
-            [0.0, data["depth"]["fy"], data["depth"]["ppy"]],
-            [0.0, 0.0, 1.0],
-        ],
-    )
-    np.savetxt(os.path.join(save_path, f"K.txt"), intrinsic)
-
-    pred_pose = est.register_any6d(
-        K=intrinsic, rgb=color, depth=depth, ob_mask=mask, iteration=5, name=f"demo"
-    )
-
-    print(pred_pose)
+        print(pred_pose)
+        predictions.append(
+            dict(
+                scene_id=sample["scene_id"],
+                im_id=sample["im_id"],
+                obj_id=obj_id,
+                gt_id=sample["gt_id"],
+                R=pred_pose[:3, :3],
+                t=pred_pose[:3, 3],
+                time=-1,
+            )
+        )
+    df = pd.DataFrame(predictions)
+    df.to_csv(os.path.join(pose_out, "predictions.csv"), index=False)
--- run.sh
+++ run.sh
@@ -1,10 +1,32 @@
-python run_ho3d_anchor.py \
-  --anchor_folder ./anchor_results/dexycb_reference_view_ours \
-  --ycb_model_path ./dataset/ho3d/YCB_Video_Models \
-  --img_to_3d
+export CUDA_VISIBLE_DEVICES=7
+
+# python run_ho3d_anchor.py \
+#   --anchor_folder ./anchor_results/dexycb_reference_view_ours \
+#   --ycb_model_path ./dataset/ho3d/YCB_Video_Models \
+#   --img_to_3d
 
 
 # python run_ho3d_query.py \
 #   --anchor_path anchor_results/dexycb_reference_view_ours \
 #   --hot3d_data_root dataset/ho3d \
 #   --ycb_model_path dataset/ho3d/YCB_Video_Models
+
+
+# predict linemod
+
+python predict_result.py \
+  --dataset lm \
+  --datasets_path data/BOP \
+  --split test \
+  --split_type none \
+  --img_to_3d 
+
+
+# predict ycbv
+
+# python predict_result.py \
+#   --dataset ycbv \
+#   --datasets_path data/BOP \
+#   --split test \
+#   --split_type none \
+#   --img_to_3d 
